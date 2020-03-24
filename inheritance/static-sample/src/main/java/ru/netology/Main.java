a82c2ed9b3af783ab3c4274ca79f170281c4e200
coursar <coursar@localhost>
2020-03-24 22:02:33 +0500

    feat(inheritance): static sample

---
+++ inheritance/static-sample/src/main/java/ru/netology/Main.java
@@ -0,0 +1,8 @@
+package ru.netology;
+
+public class Main {
+  public static void main(String[] args) {
+    double result = Math.sin(Math.PI / 2);
+    System.out.println(result);
+  }
+}
---
+++ inheritance/static-sample/src/test/java/ru/netology/SampleTest.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SampleTest {
+  @Test
+  public void shouldPass() {
+    assertTrue(true);
+  }
+}
+
