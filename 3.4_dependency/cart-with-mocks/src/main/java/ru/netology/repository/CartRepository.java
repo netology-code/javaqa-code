72090ce76d5d720c70141fa6cd7b5c4ec52d6ab6
coursar <coursar@gmail.com>
2020-11-29 20:54:45 +0300

    refactoring(dependency): CartRepository simplification

--- 3.4_dependency/cart-with-mocks/src/main/java/ru/netology/repository/CartRepository.java
+++ 3.4_dependency/cart-with-mocks/src/main/java/ru/netology/repository/CartRepository.java
@@ -18,46 +18,38 @@ public class CartRepository {
   }
 
   public void save(PurchaseItem item) {
-    try {
-      HttpRequest request = HttpRequest.newBuilder(URI.create(dbUrl))
-          .header("Content-Type", "application/json")
-          .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(item)))
-          .build();
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      if (response.statusCode() < 200 || response.statusCode() > 299) {
-        throw new RuntimeException("request didn't complete successfully");
-      }
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
+    HttpRequest request = HttpRequest.newBuilder(URI.create(dbUrl))
+        .header("Content-Type", "application/json")
+        .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(item)))
+        .build();
+    makeRequest(request);
   }
 
   public PurchaseItem[] findAll() {
-    try {
-      HttpRequest request = HttpRequest.newBuilder(URI.create(dbUrl))
-          .header("Accept", "application/json")
-          .GET()
-          .build();
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      if (response.statusCode() < 200 || response.statusCode() > 299) {
-        throw new RuntimeException("request didn't complete successfully");
-      }
-      return gson.fromJson(response.body(), PurchaseItem[].class);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
+    HttpRequest request = HttpRequest.newBuilder(URI.create(dbUrl))
+        .header("Accept", "application/json")
+        .GET()
+        .build();
+    HttpResponse<String> response = makeRequest(request);
+    return gson.fromJson(response.body(), PurchaseItem[].class);
   }
 
   public void removeById(int id) {
+    HttpRequest request = HttpRequest.newBuilder(URI.create(dbUrl + "/" + id))
+        .header("Content-Type", "application/json")
+        .DELETE()
+        .build();
+    makeRequest(request);
+  }
+
+  private HttpResponse<String> makeRequest(HttpRequest request) {
     try {
-      HttpRequest request = HttpRequest.newBuilder(URI.create(dbUrl + "/" + id))
-          .header("Content-Type", "application/json")
-          .DELETE()
-          .build();
       HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
       if (response.statusCode() < 200 || response.statusCode() > 299) {
         throw new RuntimeException("request didn't complete successfully");
       }
+
+      return response;
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
